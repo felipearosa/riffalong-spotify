98d700e22901d03d9bb9b58e15019b8638055b30
Felipe <felipe.arosa@gmail.com>
2023-01-10 19:42:20 -0300

    added footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom"
-
+import IgLogo from '../../imgs/Instagram_Glyph_White.svg'
 import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles['footer-links']}>
-        <a href="https://github.com/felipearosa/riff_along" target="_blank"><i className="fab fa-github"></i></a>
-        <a href="https://www.instagram.com/theriffalong/" target="_blank"><i className="fab fa-instagram"></i></a>
-        <Link to='/privacy' className={styles.privacy} />
+        <a href="https://www.instagram.com/theriffalong/" target="_blank"><img className={styles['social-media']} src={IgLogo} /></a>
+        <Link to='/privacy' className={styles.privacy} >Privacy Policy</Link>
       </div>
       <div className={styles['footer-copyright']}>
-        Master you song <i class="fas fa-music"></i> with The RiffAlong
+        The RiffAlong
       </div>
     </div>
 
